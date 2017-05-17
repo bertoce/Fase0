7abc921e76222f47c331f834bca075de9862a8ec
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-05-17 17:50:24 -0500

    todos los archivos del primer dia

---
+++ MetodosString.rb
@@ -0,0 +1,27 @@
+payment = "Welcome, Visa Credit Card has been processed"
+
+# print "Credit card has been charged" if payment includes the word Visa
+
+if payment.index('Visa') == 9
+
+p "Credit card has been charged" 
+
+# for any other case "We only accept visa credit cards"
+
+else
+  puts "We only accept visa credit cards"
+
+end
+
+
+
+name = "Ricardo"
+age =19
+
+if name == "Humberto" && age >= 18  
+  p "Welcome"
+
+else
+  p "No eres Mayor de Edad"
+
+end
